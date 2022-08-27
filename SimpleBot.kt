cb720d8d601c23c44f4029aa28f673559a9713d0
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-27 08:12:51 +0200

    stage 4 finished

--- SimpleBot.kt
+++ SimpleBot.kt
@@ -16,11 +16,18 @@ fun main() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    // reading all remainders
-    val num1 = readln().toInt()
-    val num2 = readln().toInt()
-    val num3 = readln().toInt()
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-    val age = (num1 * 70 + num2 * 21 + num3 * 15) % 105
-    println("Your age is ${age}; that's a good time to start programming!")
+    println("Your age is " + age + "; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    val num = readln().toInt()
+    for (n in 0..num) {
+        println("$n!")
+    }
+
+    println("Completed, have a nice day!")
 }
